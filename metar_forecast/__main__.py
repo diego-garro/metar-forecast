79f2ec202a3ae6dd683c4fb313ab479a8a4710da
diego-garro <diego.garromolina@yahoo.com>
2021-02-23 23:36:46 -0600

    download command implemented

--- metar_forecast/__main__.py
+++ metar_forecast/__main__.py
@@ -1,6 +1,7 @@
 import click
 from click.decorators import argument
 
+from .downloader import download_metars_by_year, download_most_recent_metar
 from .models.logger_model import logger
 from .parser import parse_metars_from_file, write_csv
 
@@ -32,9 +33,9 @@ def parse_metars(station: str, start_year: int, end_year: int):
 
 
 @forecast.command()
-@click.option('-s', '--start-year', type=click.INT, help='The start year to process')
-@click.option('-e', '--end-year', type=click.INT, help='The end year to process')
-@click.argument('station', type=click.STRING)
+@click.option("-s", "--start-year", type=click.INT, help="The start year to process")
+@click.option("-e", "--end-year", type=click.INT, help="The end year to process")
+@click.argument("station", type=click.STRING)
 def export(station: str, start_year: int, end_year: int):
     if start_year is not None and end_year is not None:
         for dic in write_csv(station.upper(), start_year=start_year, end_year=end_year):
@@ -48,6 +49,25 @@ def export(station: str, start_year: int, end_year: int):
     else:
         for dic in write_csv(station.upper()):
             pass
+
+
+@forecast.command()
+@click.option("-m", "--most-recent", is_flag=True)
+@click.option("-s", "--start-year", type=click.INT, help="The start year to process")
+@click.option("-e", "--end-year", type=click.INT, help="The end year to process")
+@click.argument("station", type=click.STRING, required=True)
+def download(most_recent: bool, station: str, start_year: int, end_year: int):
+    if most_recent:
+        download_most_recent_metar(station)
+    else:
+        if start_year is not None and end_year is not None:
+            download_metars_by_year(station, start_year=start_year, end_year=end_year)
+        elif start_year is not None:
+            download_metars_by_year(station, start_year=start_year)
+        elif end_year is not None:
+            download_metars_by_year(station, end_year=end_year)
+        else:
+            download_metars_by_year(station)
 
 
 if __name__ == "__main__":
